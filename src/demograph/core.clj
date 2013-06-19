d126b4c9ffe9f398d7883b922c14d10b68f591b0
Ray Willig <ray@thefreshdiet.com>
2013-06-19 16:23:35 -0400

    checking in

--- src/demograph/core.clj
+++ src/demograph/core.clj
@@ -1,16 +1,18 @@
 (ns demograph.core
   (:require [clojure.java.io :as io]
-            [clojure.data.csv :as csv]))
+            [clojure.data.csv :as csv])
+  (:require [clojure.string :refer [trim]])
+)
 
 (def third #(nth % 2))
 (def fourth #(nth % 3))
-(def extract #(str (first %) (second %) (third %) (fourth %)))
+(def extract #(str (trim (first %)) (trim (second %)) (trim (third %)) (trim (fourth %))))
 
 (defn read-master-file
  [file-name]
  (with-open [rdr (io/reader file-name)]
    (doall 
-    (mapv (fn[x] (extract x))(csv/read-csv rdr)))))
+    (into #{} (map extract (csv/read-csv rdr))))))
 
 (defn filter-data-file
  [in-file out-file master-list]
@@ -18,4 +20,23 @@
      (with-open [rtr (io/writer out-file)]
        (csv/write-csv rtr (take 1 (csv/read-csv rdr)))
        (doall
-         (filter #(some (hash-set (extract %)) master-list)(csv/read-csv rdr)))))) 
+         (csv/write-csv rtr (filter #(get master-list (extract %))(csv/read-csv rdr))))))) 
+
+(defn take-last-n-from-csv
+  [n in-file out-file]
+    (with-open [rdr (io/reader in-file)]
+      (with-open [rtr (io/writer out-file)]
+        (csv/write-csv rtr (take 1 (csv/read-csv rdr)))
+	(csv/write-csv rtr (take-last n (csv/read-csv rdr))))))
+
+(defn split-csv-files
+ [in-file & out-files]
+   (let [rdr  (io/reader in-file)
+         head (take 1 (csv/read-csv rdr))
+         outs (map (fn[x] (io/writer x)) out-files)]
+    (mapv #(csv/write-csv % head) outs)
+    (mapv #(.close %) outs)))
+
+
+
+;   (while (let [b (take 1 (csv/read-csv rdr))](not (nil? b))(csv/write-csv (nth outs (mod (Integer/parseInt (first b)) (count outs))) b)))
